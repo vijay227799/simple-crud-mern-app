299364f26d108879e88b8714abf04e29143bc406
vijay227799 <121456423+vijay227799@users.noreply.github.com>
2026-08-01 19:24:16 +0530

    Fix typo in config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const MONGO_URI = "mongodb+srv://vdr117799:<PassWord>@backenddb.x4mwhrb.mongodb.net/books-collection?appName=BackendDB";
+export const MONGO_URI = "mongodb+srv://vdr117799:<Password>@backenddb.x4mwhrb.mongodb.net/books-collection?appName=BackendDB";
